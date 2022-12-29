07a67a26535152cf8fcea417748bd720de8f8861
Grace0124 <lyuhan@gmail.com>
2022-12-29 12:04:52 -0500

    Added change number of grid squares feature.

--- javascript.js
+++ javascript.js
@@ -1,28 +1,40 @@
-
-
 const container = document.querySelector(".container");
-console.log(container);
 
-
-for (let x = 0; x < 16*16; x++) {
+function initializer(){
+  for (let x = 0; x < 256; x++) {
     const grid_square = document.createElement('div')
     grid_square.classList.add("grid_square");
     container.appendChild(grid_square);
+  }
 }
 
-const grid_squares = document.querySelectorAll("#grid_square");
+function addHover() {
+  const grid_squares = document.querySelectorAll(".grid_square");
+  grid_squares.forEach(grid_square => grid_square.addEventListener('mousemove', function(e){
+  grid_square.classList.add('hovered');
+}));
+}
 
+function changeGrid() {
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
 
-document.addEventListener('mousemove', function checkHover() {
-    const hovered = isHover(myDiv);
-    if (hovered !== checkHover.hovered) {
-      console.log(hovered ? 'hovered' : 'not hovered');
-      checkHover.hovered = hovered;
-    }
-  });
+  let r = document.querySelector(':root');
+  let squares_per_side = prompt("How many squares per side do you want?");
+  r.style.setProperty('--num-squares', squares_per_side);
+  
 
+  for (let x = 0; x < squares_per_side*squares_per_side; x++) {
+    const grid_square = document.createElement('div')
+    grid_square.classList.add("grid_square");
+    container.appendChild(grid_square);
+  
+  addHover();
+}
+}
 
+initializer();
+addHover();
 
-// document.addEventListener('mousemove', function checkHover() {
-//     c
-// })
+//cry
